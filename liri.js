e19442d0c29b149de56e8fda64ab56915652b792
liz-rondone <liz.rondone@northwestern.edu>
2017-07-18 13:21:08 -0500

    tuesday one

--- liri.js
+++ liri.js
@@ -21,23 +21,45 @@
 	// default: "Mr. Nobody" 
 
 // do-what-it-says
+	// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+	// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+	// Feel free to change the text in that document to test out the feature for other commands.
+
 
 
 /********** VARIABLES **********/
 var fs = require("fs");
 var request = require("request");
-// Grab the movieName which will always be the third node argument.
-var movieName = process.argv[2];
-// Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=  ";
-	// This line is just to help us debug against the actual URL.
-	// console.log(queryUrl);
+var spotify = require('spotify');
 var twitter = require('twitter');
+var action = process.argv[2];
+var movieName = process.argv[3];
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=  ";
+
+
+
+/********** SWITCH ACTION **********/
+switch(action) {
+	case "movie-this":
+	omdb();
+	break;
+
+	case "spotify-this-song":
+	spotify();
+	break;
+
+	// case "my-tweets":
+	// twitter();
+	// break;
+}
+
 
 
 /********** OMDb **********/
 // If the request is successful
-request(queryUrl, function(error, response, body) {
+request(queryUrl, function omdb(error, response, body) {
+
+	// If information is correctly entered 
 	if (!error && response.statusCode === 200) {
 		// Parse the body of the site and recover just the requested info
     	console.log("Movie Title: " + JSON.parse(body).Title);
@@ -49,10 +71,18 @@ request(queryUrl, function(error, response, body) {
     	console.log("Plot: " + JSON.parse(body).Plot);
     	console.log("Actors: " + JSON.parse(body).Actors);
 	}
+	console.log("sorry");
 })
 
 
+
 /********** SPOTIFY **********/
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function spotify(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+});
 
 
 
